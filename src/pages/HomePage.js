550f4fa9b711ba9854832e59ec636cdf394f9d8d
Peter Wang <wangpeter588@gmail.com>
2022-01-30 15:34:53 +0800

    20210130_SSR-Router_tutorial

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => (
+  <div>
+    <h1>HomePage</h1>
+    <h2>Hello! I am HomePage!</h2>
+  </div>
+)
+
+export default HomePage
---
+++ src/pages/OtherPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const OtherPage = () => (
+  <div>
+    <h1>OtherPage</h1>
+    <button onClick={() => console.log("click me")}>click me</button>
+  </div>
+)
+
+export default OtherPage
